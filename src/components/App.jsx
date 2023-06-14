b5ca34115349da9df762294c355dbbe2a8426dfe
chloezhouny <chloezhouny@gmail.com>
2023-06-14 11:36:33 -0700

    file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => <div>App</div>;
+import useStyles from './styles';
+import { Actors, Navbar, Movies, MovieInformation, Profile } from '.';
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
